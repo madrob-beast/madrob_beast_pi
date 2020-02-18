6b185e60bda5219f3862471fe8cabe3eee731ca5
João Pereira <95joaopereira@gmail.com>
2020-02-18 15:02:11 +0000

    Ignore event order in capability level PI

--- madrob_beast_pi/madrob/capability_level.py
+++ madrob_beast_pi/madrob/capability_level.py
@@ -27,28 +27,21 @@ def performance_indicator(preprocessed_filenames_dict, testbed_conf, output_dir,
     robot_approach_events = events_df.loc[events_df['events_sequence'] == 'humanoid_approaches_the_door_on_{}_side'.format(approach_side)]
     if len(robot_approach_events) > 0:
         capability_level += 1
-        robot_approach_timestamp = robot_approach_events.iloc[0]['timestamp']
 
-        # Handle is touched event
-        handle_is_touched_events = events_df.loc[events_df['events_sequence'] == 'handle_is_touched']
-        if len(handle_is_touched_events) > 0:
-            handle_is_touched_timestamp = handle_is_touched_events.iloc[0]['timestamp']
-            if handle_is_touched_timestamp > robot_approach_timestamp:
-                capability_level += 1
+    # Handle is touched event
+    handle_is_touched_events = events_df.loc[events_df['events_sequence'] == 'handle_is_touched']
+    if len(handle_is_touched_events) > 0:
+        capability_level += 1
 
-                # Move to destination event
-                robot_moves_to_dest_events = events_df.loc[events_df['events_sequence'] == 'humanoid_moves_to_{}_side'.format(destination_side)]
-                if len(robot_moves_to_dest_events) > 0:
-                    robot_moves_to_dest_timestamp = robot_moves_to_dest_events.iloc[0]['timestamp']
-                    if robot_moves_to_dest_timestamp > handle_is_touched_timestamp:
-                        capability_level += 1
+    # Move to destination event
+    robot_moves_to_dest_events = events_df.loc[events_df['events_sequence'] == 'humanoid_moves_to_{}_side'.format(destination_side)]
+    if len(robot_moves_to_dest_events) > 0:
+        capability_level += 1
 
-                        # Door closes event
-                        door_closes_events = events_df.loc[events_df['events_sequence'] == 'door_closes']
-                        if len(door_closes_events) > 0:
-                            last_door_close_timestamp = door_closes_events.iloc[-1]['timestamp']
-                            if last_door_close_timestamp > robot_moves_to_dest_timestamp:
-                                capability_level += 1
+    # Door closes event
+    door_closes_events = events_df.loc[events_df['events_sequence'] == 'door_closes']
+    if len(door_closes_events) > 0:
+        capability_level += 1
 
     # Write result yaml file
     filepath = path.join(output_dir, 'capability_level_%s.yaml' % (start_time.strftime('%Y%m%d_%H%M%S')))
